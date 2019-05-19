03c04c5c3bacd5a4101cfa142f4939a63645cbd8
javad <javad@192.168.219.102>
2019-05-19 16:38:07 +0900

    집에서 책으로 한 부분

---
+++ src/Example1.java
@@ -0,0 +1,24 @@
+
+public class Example1
+							{
+	public static void main(String[] args) {
+		
+		int a, b;
+		int result;
+		
+		a = 100;
+		b = 50;
+		
+		result = a + b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a - b;
+		System.out.println(a + "-" + b + "=" + result);
+		
+		result = a * b;
+		System.out.println(a + "*" + b + "=" + result);
+		
+		result = a / b;
+		System.out.println(a + "/" + b + "=" + result);
+	}
+}
---
+++ src/Example2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Example2
+							{
+	public static void main(String[] args) {
+		
+		int a, b;
+		int result;
+		
+		Scanner s = new Scanner(System.in);
+		a = s.nextInt();
+		b = s.nextInt();
+		
+		result = a + b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a - b;
+		System.out.println(a + "-" + b + "=" + result);
+		
+		result = a * b;
+		System.out.println(a + "*" + b + "=" + result);
+		
+		result = a / b;
+		System.out.println(a + "/" + b + "=" + result);
+	}
+}
